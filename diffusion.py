88f080a7edbed45c32d91ac677133173ecf7030a
Burvye <thebuffonion@gmail.com>
2026-08-05 19:06:19 -0400

    docs: format comment

--- diffusion.py
+++ diffusion.py
@@ -7,8 +7,9 @@ def linear_beta_schedule(timesteps: int) -> torch.Tensor:
     add noise at each step
     """
     betas = linear_beta_schedule(TIMESTEPS)
-    alphas = 1.0 - betas                             # signal surviving ONE step
-    alphas_cumprod = torch.cumprod(alphas, dim=0)    # ...surviving ALL steps up to t
+    alphas = 1.0 - betas # signal surviving ONE step
+    # ... all steps up to t
+    alphas_cumprod = torch.cumprod(alphas, dim=0)
 
     sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
     sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod)
